22434522d3dd51d93d73a05803286b2f8fee5536
A G <16706771+andrewgin@users.noreply.github.com>
2023-04-01 22:30:12 +0100

    add JSON representations for set, slice and datetime objects

--- make_observations.py
+++ make_observations.py
@@ -184,6 +184,28 @@ def make_observations():
         file.write(obs_bundle.serialize())
 
 
+class PythonObjectEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, set):
+            return list(o)
+        elif isinstance(o, slice):
+            if o.step:
+                components = [o.start, o.stop, o.step]
+            elif o.start:
+                components = [o.start, o.stop]
+            else:
+                components = [o.stop]
+
+            slice_string = ':'.join(
+                str(comp) if comp else '' for comp in components
+            )
+            return f'[{slice_string}]'
+        elif isinstance(o, (datetime)):
+            return o.isoformat()
+
+        return json.JSONEncoder.default(self, o)
+
+
 def make_example_dicts():
     """
         process the patterns here in order to create the dicts that described them
@@ -223,7 +245,7 @@ def make_example_dicts():
                 start_stop['stop'] = start_stop['stop'].replace(tzinfo=pytz.utc)
                 dict_tree['pattern']['observation']['qualifiers'][0]['start_stop'] = start_stop
 
-            json.dump(dict_tree['pattern'], file)
+            json.dump(dict_tree['pattern'], file, cls=PythonObjectEncoder)
 
 
 # if this file is run directly, then start here

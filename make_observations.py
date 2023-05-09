4904b9cb2263d8c96a20c024e352b63d7d5187f5
BrettForbes <brett@cloudaccelerator.co>
2023-05-09 22:29:43 +1000

    Setting up the tql conversion, but didnt know how to make a new branch

--- make_observations.py
+++ make_observations.py
@@ -2,6 +2,7 @@ import pytz
 import json
 from datetime import datetime, timedelta
 import logging
+import yaml
 
 from stix2 import DomainName, File, IPv4Address
 from stix2 import (ObjectPath, EqualityComparisonExpression, ObservationExpression,
@@ -30,42 +31,50 @@ def make_patterns():
 
     '''
     patterns = {}
+    statements = {}
     # Equality Comparison expressions
     lhs = ObjectPath("domain-name", ["value"])
     patterns["state_1"] = ObservationExpression(EqualityComparisonExpression(lhs, "site.of.interest.zaz"))
-    print("statement 1\t{}\n".format(patterns["state_1"]))
+    statements["state_1"] = "statement 1\t{}\n".format(patterns["state_1"])
+    print(statements["state_1"])
 
     lhs = ObjectPath("file", ["parent_directory_ref", "path"])
     patterns["state_2"] = ObservationExpression(EqualityComparisonExpression(lhs, "C:\\Windows\\System32"))
-    print("statement 2\t{}\n".format(patterns["state_2"]))
+    statements["state_2"] = "statement 2\t{}\n".format(patterns["state_2"])
+    print(statements["state_2"])
 
     # Greater-than Comparison expressions
     lhs = ObjectPath("file", ["extensions", "windows-pebinary-ext", "sections[*]", "entropy"])
     patterns["state_3"] = ObservationExpression(GreaterThanComparisonExpression(lhs, FloatConstant("7.0")))
-    print("statement 3\t{}\n".format(patterns["state_3"]))
+    statements["state_3"] = "statement 3\t{}\n".format(patterns["state_3"])
+    print(statements["state_3"])
 
     # IsSubset Comparison expressions
     lhs = ObjectPath("network-traffic", ["src_ref", "value"])
     patterns["state_4"] = ObservationExpression(
         IsSubsetComparisonExpression(lhs, StringConstant("2001:0db8:85a3:0000:0000:8a2e:0370:7334/64")))
-    print("statement 4\t{}\n".format(patterns["state_4"]))
+    statements["state_4"] = "statement 4\t{}\n".format(patterns["state_4"])
+    print(statements["state_4"])
 
     # Compound Observation Expressions
     # AND boolean
     ece3 = EqualityComparisonExpression(ObjectPath("email-message", ["sender_ref", "value"]), "jdoe@example.com")
     ece4 = EqualityComparisonExpression(ObjectPath("email-message", ["subject"]), "Conference Info")
     patterns["state_5"] = ObservationExpression(AndBooleanExpression([ece3, ece4]))
-    print("statement 5 (AND) \n{}\n".format(patterns["state_5"]))
+    statements["state_5"] = "statement 5 (AND) \n{}\n".format(patterns["state_5"])
+    print(statements["state_5"])
     # AND boolean
     ece3B = EqualityComparisonExpression(ObjectPath("domain-name", ["resolves_to_refs", "value"]), "198.51.100.3")
     ece4B = EqualityComparisonExpression(ObjectPath("domain-name", ["value"]), "site.of.interest.zaz")
     patterns["state_6"] = ObservationExpression(AndBooleanExpression([ece4B, ece3B]))
-    print("statement 6 (AND) v2 \n{}\n".format(patterns["state_6"]))
+    statements["state_6"] = "statement 6 (AND) v2 \n{}\n".format(patterns["state_6"])
+    print(statements["state_6"])
     # OR boolean
     ece5 = EqualityComparisonExpression(ObjectPath("url", ["value"]), "http://example.com/foo")
     ece6 = EqualityComparisonExpression(ObjectPath("url", ["value"]), "https://example.com/research/index.html")
     patterns["state_7"] = ObservationExpression(OrBooleanExpression([ece5, ece6]))
-    print("statement 7 (OR) \n{}\n".format(patterns["state_7"]))
+    statements["state_7"] = "statement 7 (OR) \n{}\n".format(patterns["state_7"])
+    print(statements["state_7"])
 
     # ( AND ) OR ( OR ) observation
     ece20 = ObservationExpression(EqualityComparisonExpression(ObjectPath("file", ["name"]), "foo.dll"))
@@ -78,7 +87,8 @@ def make_patterns():
     aoe = ParentheticalExpression(AndObservationExpression([ece20, ece21]))
     obe2 = ObservationExpression(OrBooleanExpression([ece22, ece23]))
     patterns["state_8"] = OrObservationExpression([aoe, obe2])
-    print("statement 8 (AND,OR,OR) \n{}\n".format(patterns["state_8"]))
+    statements["state_8"] = "statement 8 (AND,OR,OR) \n{}\n".format(patterns["state_8"])
+    print(statements["state_8"])
 
     # FOLLOWED-BY
     ece10 = ObservationExpression(EqualityComparisonExpression(ObjectPath("file", ["hashes", "SHA-256"]), HashConstant(
@@ -86,7 +96,8 @@ def make_patterns():
     ece11 = ObservationExpression(
         EqualityComparisonExpression(ObjectPath("win-registry-key", ["key"]), "hkey_local_machine\\system\\bar\\foo"))
     patterns["state_9"] = FollowedByObservationExpression([ece10, ece11])
-    print("statement 9 (FollowedBy) \n{}\n".format(patterns["state_9"]))
+    statements["state_9"] = "statement 9 (FollowedBy) \n{}\n".format(patterns["state_9"])
+    print(statements["state_9"])
 
     # Qualified Observation Expressions
     # WITHIN
@@ -97,21 +108,24 @@ def make_patterns():
     fbe = FollowedByObservationExpression([ece10, ece11])
     par = ParentheticalExpression(fbe)
     patterns["state_10"] = QualifiedObservationExpression(par, WithinQualifier(300))
-    print("statement 10 (WITHIN) \n{}\n".format(patterns["state_10"]))
+    statements["state_10"] = "statement 10 (WITHIN) \n{}\n".format(patterns["state_10"])
+    print(statements["state_10"])
     # REPEATS, WITHIN
     ece12 = EqualityComparisonExpression(ObjectPath("network-traffic", ["dst_ref", "type"]), "domain-name")
     ece13 = EqualityComparisonExpression(ObjectPath("network-traffic", ["dst_ref", "value"]), "example.com")
     abe2 = ObservationExpression(AndBooleanExpression([ece12, ece13]))
     patterns["state_11"] = QualifiedObservationExpression(QualifiedObservationExpression(abe2, RepeatQualifier(5)),
                                                           WithinQualifier(180))
-    print("statement 11 (REPEAT, WITHIN) \n{}\n".format(patterns["state_11"]))
+    statements["state_11"] = "statement 11 (REPEAT, WITHIN) \n{}\n".format(patterns["state_11"])
+    print(statements["state_11"])
     # START, STOP
     ece14 = ObservationExpression(EqualityComparisonExpression(ObjectPath("file", ["name"]), "foo.dll"))
     ssq = StartStopQualifier(TimestampConstant('2016-06-01T00:00:00Z'), TimestampConstant('2016-07-01T00:00:00Z'))
     patterns["state_12"] = QualifiedObservationExpression(ece14, ssq)
-    print("statement 12 (START-STOP) \n{}\n".format(patterns["state_12"]))
+    statements["state_12"] = "statement 12 (START-STOP) \n{}\n".format(patterns["state_12"])
+    print(statements["state_12"])
 
-    return patterns
+    return patterns, statements
 
 
 def make_observations():
@@ -214,7 +228,7 @@ def make_example_dicts():
         and thensave them in the patterns/results directory, with the filename=dict key,
         so state_1.json etc.
     """
-    patterns = make_patterns()
+    patterns, statements = make_patterns()
     for key, pattern in patterns.items():
         logging.info(f'Processing statement {key}')
         pattern_tree = TreePattern(str(pattern))
@@ -248,6 +262,18 @@ def make_example_dicts():
                 dict_tree['pattern']['observation']['qualifiers'][0]['start_stop'] = start_stop
 
             json.dump(dict_tree['pattern'], file, cls=PythonObjectEncoder)
+
+    for key, statement in statements:
+        print('======================================================================')
+        print(f'------------------- {key} -------------------')
+        make_tql(key, statement)
+
+
+def make_tql(key, statement):
+    """
+    make the tql here from each yaml file and print the original statement vs the tql match statement
+    """
+    pass
 
 
 # if this file is run directly, then start here
